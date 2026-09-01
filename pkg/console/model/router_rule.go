a68f609bcecc9c89132fe421157c6d8e1731cc10
白 <575121043@qq.com>
2026-09-01 13:47:57 +0800

    feat: add affinity and script router APIs

---
+++ pkg/console/model/router_rule.go
@@ -0,0 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package model
+
+import meshproto "github.com/apache/dubbo-admin/api/mesh/v1alpha1"
+
+type RouterRuleSearchResp struct {
+	CreateTime string `json:"createTime"`
+	Enabled    bool   `json:"enabled"`
+	RuleName   string `json:"ruleName"`
+	Scope      string `json:"scope"`
+}
+
+type AffinityRuleInput struct {
+	ConfigVersion string                   `json:"configVersion"`
+	Scope         string                   `json:"scope"`
+	Key           string                   `json:"key"`
+	Runtime       bool                     `json:"runtime"`
+	Enabled       bool                     `json:"enabled"`
+	AffinityAware *meshproto.AffinityAware `json:"affinityAware"`
+}
+
+// ToProto maps the public affinityAware field back to the internal proto field
+// name used by the generated API type.
+func (i *AffinityRuleInput) ToProto() *meshproto.AffinityRoute {
+	return &meshproto.AffinityRoute{
+		ConfigVersion: i.ConfigVersion,
+		Scope:         i.Scope,
+		Key:           i.Key,
+		Runtime:       i.Runtime,
+		Enabled:       i.Enabled,
+		Affinity:      i.AffinityAware,
+	}
+}
+
+// GenAffinityRuleResp exposes affinityAware instead of the internal affinity
+// field to keep the Console API aligned with Dubbo runtime YAML.
+func GenAffinityRuleResp(data *meshproto.AffinityRoute) *CommonResp {
+	if data == nil {
+		return NewSuccessResp(nil)
+	}
+	return NewSuccessResp(AffinityRuleInput{
+		ConfigVersion: data.ConfigVersion,
+		Scope:         data.Scope,
+		Key:           data.Key,
+		Runtime:       data.Runtime,
+		Enabled:       data.Enabled,
+		AffinityAware: data.Affinity,
+	})
+}
